e6843aad2ef5cd45e878464a1c0eb8f4a0367e3b
KARTIK-44 <kartikpatilcs@gmail.com>
2026-01-11 22:25:55 +0530

    COMPLETE

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,6 @@
+console.log("ENV:", import.meta.env);
+console.log("KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
